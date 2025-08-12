7a397375f98646545bad0efe8dd17e2c3d6d02e0
Mushir Ali <mushirmahdi123@gmail.com>
2025-08-13 02:39:13 +0530

    changed thumbnail error

--- backend/controllers/resumeControllers.js
+++ backend/controllers/resumeControllers.js
@@ -148,8 +148,8 @@ export const deleteResume = async(req,res)=>{
         const uploadsFolder = path.join(process.cwd(),'uploads')
 
         //  delete the thumbnail or resume
-        if(resume.thumbnailLink){
-            const oldThumbnail = path.join(uploadsFolder,path.basename(resume.thumbnailLink))
+        if(resume.thumbnail){
+            const oldThumbnail = path.join(uploadsFolder,path.basename(resume.thumbnail))
             if (fs.existsSync(oldThumbnail)){
                 fs.unlinkSync(oldThumbnail);
             }
