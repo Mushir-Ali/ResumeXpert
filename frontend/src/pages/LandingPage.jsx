c6f37015edb4c91af2628f005b96f7b9e586e065
Mushir Ali <mushirmahdi123@gmail.com>
2025-10-13 23:39:26 +0530

    Naming check

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -33,7 +33,7 @@ const LandingPage = () => {
                         <LayoutTemplate className={landingPageStyles.logoIconInner} />
                     </div>
                     <span className={landingPageStyles.logoText}>
-                        ElevateU
+                        ElevateU 
                     </span>
                 </div>
                 {/* Mobile menu button */}
